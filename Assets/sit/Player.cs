98988f5a5207bec8b58e104f378f029f8de81af3
tawil985 <0919michael985@gmail.com>
2022-12-19 10:52:52 +0800

    跳

--- Assets/sit/Player.cs
+++ Assets/sit/Player.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    Rigidbody2D rigid2D;          
+    float jumpForce = 680.0f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigid2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rigid2D.AddForce(transform.up * jumpForce);
+        }
     }
 }
