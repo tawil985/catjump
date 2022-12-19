fb58830081c9abfacbb5ecdb655bf128698c1261
tawil985 <0919michael985@gmail.com>
2022-12-19 11:09:31 +0800

    終點

--- Assets/sit/Player.cs
+++ Assets/sit/Player.cs
@@ -6,6 +6,8 @@ public class Player : MonoBehaviour
 {
     Rigidbody2D rigid2D;          
     float jumpForce = 680.0f;
+    float walkForce = 30.0f;     
+    float maxWalkSpeed = 2.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,6 +20,22 @@ public class Player : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space))
         {
             rigid2D.AddForce(transform.up * jumpForce);
+        }
+        int key = 0;
+        if (Input.GetKey(KeyCode.RightArrow)) key = 1;
+        if (Input.GetKey(KeyCode.LeftArrow)) key = -1;
+
+        
+        float speedx = Mathf.Abs(rigid2D.velocity.x);
+
+        
+        if (speedx < maxWalkSpeed)
+        {
+            rigid2D.AddForce(transform.right * key * walkForce);
+        }
+        if (key != 0)
+        {
+            transform.localScale = new Vector3(key, 1, 1);
         }
     }
 }
